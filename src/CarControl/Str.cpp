98a782198d935487d714933c89556068a6b2f99e
thomkoehler <thom.koehler@gmail.com>
2019-01-07 10:28:18 +0100

    add tokenize

---
+++ src/CarControl/Str.cpp
@@ -0,0 +1,54 @@
+
+size_t tokenize(char *str, size_t maxTokenCount, char *tokens[])
+{
+  bool inToken = false;
+  size_t tokenPos = 0;
+
+  for (;;)
+  {
+    char c = *str;
+    if (inToken)
+    {
+      switch (c)
+      {
+      case '\0':
+        return tokenPos;
+
+      case '\n':
+      case '\r':
+      case '\t':
+      case ' ':
+        inToken = false;
+        *str = '\0';
+        break;
+      }
+    }
+    else
+    {
+      switch (c)
+      {
+      case '\0':
+        return tokenPos;
+
+      case '\n':
+      case '\r':
+      case '\t':
+      case ' ':
+        break;
+
+      default:
+        tokens[tokenPos++] = str;
+
+        if (tokenPos >= maxTokenCount)
+        {
+          return tokenPos;
+        }
+
+        inToken = true;
+        break;
+      }
+    }
+
+    ++str;
+  }
+}
---
+++ src/CarControl/Str.h
@@ -0,0 +1,4 @@
+
+#pragma once
+
+size_t tokenize(char *str, size_t maxTokenCount, char *tokens[]);
