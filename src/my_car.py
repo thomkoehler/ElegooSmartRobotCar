fac6a6556f05808341fffb3462992e9f690b3b34
thomkoehler <thom.koehler@gmail.com>
2019-01-10 12:27:22 +0100

    use QGridLayout

--- src/my_car.py
+++ src/my_car.py
@@ -1,6 +1,6 @@
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QSlider
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QSlider
 
 # from car_control import CarControl
 from car_control_mock import CarControl
@@ -12,27 +12,27 @@ carControl = CarControl(MAC)
 
 app = QApplication([])
 
-layout = QVBoxLayout()
+layout = QGridLayout()
 
 forwardButton = QPushButton('Forward')
 forwardButton.clicked.connect(lambda: carControl.forward())
-layout.addWidget(forwardButton)
+layout.addWidget(forwardButton, 0, 1)
 
 backButton = QPushButton('Back')
 backButton.clicked.connect(lambda: carControl.backward())
-layout.addWidget(backButton)
+layout.addWidget(backButton, 2, 1)
 
 leftButton = QPushButton('Left')
 leftButton.clicked.connect(lambda: carControl.left())
-layout.addWidget(leftButton)
+layout.addWidget(leftButton, 1, 0)
 
 rightButton = QPushButton('Right')
 rightButton.clicked.connect(lambda: carControl.right())
-layout.addWidget(rightButton)
+layout.addWidget(rightButton, 1, 2)
 
 stopButton = QPushButton('Stop')
 stopButton.clicked.connect(lambda: carControl.stop())
-layout.addWidget(stopButton)
+layout.addWidget(stopButton, 1, 1)
 
 speedSlider = QSlider(Qt.Vertical)
 speedSlider.setFocusPolicy(Qt.StrongFocus)
@@ -44,7 +44,7 @@ speedSlider.setMaximum(255)
 speedSlider.valueChanged.connect(lambda: carControl.setSpeed(speedSlider.value()))
 speedSlider.setValue(INIT_SPEED)
 carControl.setSpeed(INIT_SPEED)
-layout.addWidget(speedSlider)
+layout.addWidget(speedSlider, 3, 0)
 
 window = QWidget()
 window.setLayout(layout)
