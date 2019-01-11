5a811b752a24b179093f994ecf25966ffb6fd8b6
thomkoehler <thom.koehler@gmail.com>
2019-01-11 14:06:45 +0100

    add DDPosSlider

--- src/my_car.py
+++ src/my_car.py
@@ -7,6 +7,7 @@ from car_control_mock import CarControl
 
 MAC = "88:3F:4A:D9:16:A8"
 INIT_SPEED = 0
+INIT_DD_POS = 90
 
 
 class MyCarWindow(QDialog):
@@ -38,12 +39,14 @@ class MyCarWindow(QDialog):
         layout.addWidget(stopButton, 1, 1)
 
         layout.addWidget(self._createSpeedSlider(), 3, 0)
+        layout.addWidget(self._createDDPosSlider(), 3, 1)
+
 
         self.setWindowTitle('My Car')
         self.setLayout(layout)
 
     def _createSpeedSlider(self):
-        layout = QVBoxLayout()
+        self._speedLabel = QLabel(str(INIT_SPEED))
 
         speedSlider = QSlider(Qt.Vertical)
         speedSlider.setFocusPolicy(Qt.StrongFocus)
@@ -55,10 +58,9 @@ class MyCarWindow(QDialog):
         speedSlider.valueChanged.connect(lambda: self._onSpeedChanged(speedSlider))
         speedSlider.setValue(INIT_SPEED)
 
-        self._speedLabel = QLabel(str(INIT_SPEED))
-
         self._carControl.setSpeed(INIT_SPEED)
 
+        layout = QVBoxLayout()
         layout.addWidget(self._speedLabel)
         layout.addWidget(speedSlider)
         widget = QWidget()
@@ -69,6 +71,33 @@ class MyCarWindow(QDialog):
         val = speedSlider.value()
         self._carControl.setSpeed(val)
         self._speedLabel.setText(str(val))
+
+    def _createDDPosSlider(self):
+        self._ddPosLabel = QLabel(str(INIT_DD_POS))
+
+        ddPosSlider = QSlider(Qt.Horizontal)
+        ddPosSlider.setFocusPolicy(Qt.StrongFocus)
+        ddPosSlider.setTickPosition(QSlider.TicksBothSides)
+        ddPosSlider.setTickInterval(5)
+        ddPosSlider.setSingleStep(5)
+        ddPosSlider.setMinimum(45)
+        ddPosSlider.setMaximum(135)
+        ddPosSlider.valueChanged.connect(lambda: self._onDDPosChanged(ddPosSlider))
+        ddPosSlider.setValue(INIT_DD_POS)
+
+        self._carControl.setDistanceDetectionPos(INIT_DD_POS)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._ddPosLabel)
+        layout.addWidget(ddPosSlider)
+        widget = QWidget()
+        widget.setLayout(layout)
+        return widget
+
+    def _onDDPosChanged(self, ddPosSlider):
+        val = 180 - ddPosSlider.value()
+        self._carControl.setDistanceDetectionPos(val)
+        self._ddPosLabel.setText(str(val))
 
 
 app = QApplication([])
